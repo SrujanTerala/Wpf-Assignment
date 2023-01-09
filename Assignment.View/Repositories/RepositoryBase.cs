3d511803aec29e52a08a6848ab3452e091d9cc47
Srujan.Terala <Srujan.Terala@geminisolutions.com>
2023-01-10 03:37:23 +0530

    Add project files.

---
+++ Assignment.View/Repositories/RepositoryBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Assignment.View.Repositories
+{
+    public abstract class RepositoryBase
+    {
+        private readonly string _connectonString;
+        public RepositoryBase()
+        {
+            _connectonString= "Data Source=(Localdb)\tuserdb;Initial Catalog=Users;Integrated Security=True;Pooling=False";
+        }
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectonString);
+        }
+    }
+}
